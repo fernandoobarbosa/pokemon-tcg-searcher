ece290fe7bd25b0c50b79d650259f59a79196120
fernandoobarbosa <fernandobarbosa1697@gmail.com>
2021-09-09 11:24:04 -0300

    botao de voltar

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,23 +1,36 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="450">
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <div>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="450">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
 
-    <v-img
-      v-if="pokemon.images !== undefined"
-      :lazy-src="pokemon.images.large"
-      :src="pokemon.images.large"
-      aspect-ratio="1"
-      class="grey lighten-2"
-      height="600"
-      width="450"
-    />
-  </v-card>
+      <v-img
+        v-if="pokemon.images !== undefined"
+        :lazy-src="pokemon.images.large"
+        :src="pokemon.images.large"
+        aspect-ratio="1"
+        class="grey lighten-2"
+        height="600"
+        width="450"
+      />
+    </v-card>
+    <div class="text-center">
+      <v-btn
+        class="ma-2"
+        outlined
+        color="indigo"
+        @click="goBackToHome"
+        v-if="pokemon.images !== undefined"
+      >
+        Go back
+      </v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -34,6 +47,9 @@ export default {
     reserve() {
       this.loading = true;
       setTimeout(() => (this.loading = false), 2000);
+    },
+    goBackToHome() {
+      this.$router.push({ path: "/" });
     },
   },
 };
